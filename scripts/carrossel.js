2cb28992068e487ec2583f5a2a34714090d08b1d
Leonardo Fonseca <leonardofs@dcc.ufrj.com.br>
2023-02-13 20:59:43 -0300

    Aplicação de Js no carrossel de imagens

---
+++ scripts/carrossel.js
@@ -0,0 +1,42 @@
+const slider = document.querySelectorAll(".slider");
+const prev = document.getElementById("prev-button");
+const next = document.getElementById("next-button");
+
+let slideAtual = 0;
+
+function escondeSlide(){
+    slider.forEach(item => item.classList.remove("on"));
+}
+
+function mostraSlide(){
+    slider[slideAtual].classList.add("on");
+}
+
+function proximoSlider(){
+    escondeSlide();
+    if(slideAtual == slider.length - 1){
+        slideAtual = 0;
+    }
+    else{
+        slideAtual++;
+    }
+
+    mostraSlide();
+}
+
+function voltaSlide(){
+    escondeSlide();
+    if(slideAtual == 0){
+        slideAtual = slider.length - 1;
+    }
+    else{
+        slideAtual--;
+    }
+
+    mostraSlide();
+}
+
+next.addEventListener("click", proximoSlider);
+prev.addEventListener("click", voltaSlide);
+
+console.log(slider);
