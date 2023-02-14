26e0d05d196044157847711f5094a5e097b47cec
Leonardo Fonseca <leonardofs@dcc.ufrj.com.br>
2023-02-13 21:03:48 -0300

    Comentários

--- scripts/carrossel.js
+++ scripts/carrossel.js
@@ -4,6 +4,8 @@ const next = document.getElementById("next-button");
 
 let slideAtual = 0;
 
+
+// Modificação dos slides acontece pela edicao das classes
 function escondeSlide(){
     slider.forEach(item => item.classList.remove("on"));
 }
@@ -12,6 +14,7 @@ function mostraSlide(){
     slider[slideAtual].classList.add("on");
 }
 
+// passa slide de acordo com a pos atual
 function proximoSlider(){
     escondeSlide();
     if(slideAtual == slider.length - 1){
